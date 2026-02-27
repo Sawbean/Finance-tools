da8ec39056cab0a0689c9eaeae30543dbc952358
Sabin Bhandari <sawbeenbhandari@gmail.com>
2026-02-27 15:43:43 +0545

    update1 5 page added

---
+++ pages/disclaimer.js
@@ -0,0 +1,28 @@
+export default function Disclaimer() {
+  return (
+    <div className="container">
+      <h1>Disclaimer</h1>
+
+      <p>
+        The information provided on ToolFinance is for general informational
+        purposes only.
+      </p>
+
+      <p>
+        While we strive to keep the calculators and content accurate and
+        up-to-date, ToolFinance makes no warranties of any kind regarding
+        accuracy, reliability, or completeness.
+      </p>
+
+      <p>
+        Any action you take based on the information found on this website is
+        strictly at your own risk.
+      </p>
+
+      <p>
+        ToolFinance will not be liable for any losses or damages in connection
+        with the use of this website.
+      </p>
+    </div>
+  );
+}
