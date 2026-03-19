117fa406103815b8dd0e6c32fab365bba476a78f
Sabin Bhandari <sawbeenbhandari@gmail.com>
2026-03-19 14:00:14 +0545

    sitemap

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/sitemap.xml",
-        destination: "/api/sitemap",
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
       },
     ];
   },
 };
+
+module.exports = nextConfig;
